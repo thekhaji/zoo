5da1c4f21c373d7544d64c7e79c3a767aa53a903
Khojiakbar <khojiakbar_kodirov@mail.ru>
2024-08-19 13:28:30 +0900

    feat: create post api request

--- src/dog/dog.controller.ts
+++ src/dog/dog.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Param, Query, Req, Res } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Query, Req, Res } from '@nestjs/common';
 import { DogService } from './dog.service';
 import { query, Request, Response } from 'express';
 
@@ -19,4 +19,12 @@ export class DogController {
         console.log("query:", query);
         return this.dogService.introduce();
     }
+
+    @Post('edit')
+    public moddiffyDetail(@Body() body: any): string{
+        console.log("body:", body);
+        return this.dogService.modifyetail();
+        
+    }
 }
+
